1735b1c4d6cbee20e3ed593d835e608392889080
ManeRomero <algoritmo717@gmail.com>
2019-07-16 00:36:52 +0200

    made api call into console

---
+++ src/components/chucknorris.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h3> {{fraseChuck}} </h3>
+      <button>Pónmelo otra vez, Chuck</button>
+      <small>{{ titulo }}</small>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "chucknorris",
+  props: {
+    titulo: String,
+    fraseChuck: String
+  }
+};
+</script>
+
+<style scoped>
+button {
+  padding: 10px;
+  font-weight: 800;
+  font-size: 1em;
+  border: 5px solid gold;
+  border-radius: 5px 3px 3px 1px;
+  background-color: transparent;
+  cursor: pointer;
+}
+
+small {
+  border-bottom: 2px solid grey;
+  padding: 5px;
+}
+
+.hello {
+  display: flex;
+  flex-direction: column;
+  max-height: 100vh;
+  justify-content: space-around;
+  width: 30%;
+  margin: auto;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="">
+    <h4>{{ frase }}</h4>
+    <img alt="Chuck Norris es el Hombre." 
+    src="http://cinedivergente.com/wp-content/uploads/2015/08/Chuck-Norris-vs-Communism.jpg">
+    <chucknorris titulo="chuck norris ahora usa Vue!" fraseChuck="getFrase()"/>
+
+  </div>
+</template>
+
+<script>
+import chucknorris from "@/components/chucknorris.vue";
+
+export default {
+  name: "home",
+  data() {
+    return {
+      frase: ""
+    };
+  },
+  mounted() {
+    this.getFrase();
+  },
+  components: {
+    chucknorris
+  },
+  methods: {
+    getFrase() {
+      let result = "";
+      fetch("https://api.chucknorris.io/jokes/random").then(resp => {
+        resp.json().then(function(data) {
+          console.log(data.value);
+          this.frase = data.value;
+        });
+      });
+    }
+  }
+};
+</script>
